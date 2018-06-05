6c1c22735547a85ed4920cd892d2ccba7e626a87
shanmugaraj022 <39941680+shanmugaraj022@users.noreply.github.com>
2018-06-05 15:01:00 +0530

    Create largestno.py

---
+++ largestno.py
@@ -0,0 +1,11 @@
+# vino
+# your code goes 
+a=int(input("enter the value1:"))
+b=int(input("enter the value2:"))
+c=int(input("enter the value3:"))
+if((a>b)and(a>c)):
+	print("a is larger")
+elif((b>c)and(b>a)):
+	print("b is larger")
+else:
+	print("c is larger")

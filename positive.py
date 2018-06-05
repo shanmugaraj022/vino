8e99428184dccc74dcc1aa58799298df27340e84
shanmugaraj022 <39941680+shanmugaraj022@users.noreply.github.com>
2018-06-05 13:22:35 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,12 @@
+# vino
+N=int(input("enter the values")
+if(N<100000):
+    if(N>0):
+        print("positive")
+    elif(N<0):
+        print("negative")
+    elif(N==0):
+        print("zero")
+ else:
+     print("over flow")
+        
